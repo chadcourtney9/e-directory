c85956791b3d80657c1a1b948a441c6029d2f47d
Chad Courtney <Chadcourtney567@gmail.com>
2021-05-02 18:33:40 -0400

    Updates

--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    getUsers: function () {
-        return axios.get("https://randomuser.me/api/?results=50&nat=us");
-    }
-};
